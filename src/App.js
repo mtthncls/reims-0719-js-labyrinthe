011e2491d0f174a3502a6257abbbdde43f11b254
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-07-10 15:22:14 +0200

    add player image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
+import Player from './Components/player';
 import './App.css';
 
 class App extends Component {
 
   render() {
     const grid = [[1, 2, 3], [4, 5, 6]]
-    
-  return (
-    grid.map(row => (
-    <div className="App" key={row}>
-      {row.map(cell => (
-      <div className="case" key={cell}>
-        id={cell}
-      </div>))}
-    </div>),
-  )
-  )}
-    }
+
+    return (
+      grid.map(row => (
+        <div className="App" key={row}>
+          {row.map(cell => (
+            <div className="case" key={cell}>
+              id={cell}
+              <Player />
+            </div>))}
+        </div>),
+      )
+    )
+  }
+}
 
 export default App;
---
+++ src/Components/player.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Player extends Component {
+    render() {
+        return (
+         <div > 
+          <img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/rick-sanchez-1-563922.png" height="100px" width="100px" alt="avatar" />
+         </div>
+       )
+    }
+}
+
+export default Player;
