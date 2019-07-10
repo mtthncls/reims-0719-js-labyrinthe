6de17119205b93a39061deb17e2a0efb3952973c
Matthieu NICOLAS <mtthncls@gmail.com>
2019-07-10 15:03:23 +0200

    adding Components folder

---
+++ src/Components/Grid.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const grid = [
+    [
+        {
+            id: 1,
+            up:,
+            down:,
+            left:,
+            right:,
+        },
+
+        {
+            id:,
+            up:,
+            down:,
+            left:,
+            right:,
+        },
+
+        {
+            id:,
+            up:,
+            down:,
+            left:,
+            right:,
+        },
+    ],
+
+    [
+        {
+            id:,
+            up:,
+            down:,
+            left:,
+            right:,
+        },
+
+        {
+            id:,
+            up:,
+            down:,
+            left:,
+            right:,
+        },
+
+        {
+            id:,
+            up:,
+            down:,
+            left:,
+            right:,
+        },
+    ]
+]
+
+
+export default 
