ffd0be2dc64678a33fa67c2d54771778c79303ef
Matthieu NICOLAS <mtthncls@gmail.com>
2019-07-12 11:15:54 +0200

    manage get key

--- src/Components/Maze.js
+++ src/Components/Maze.js
@@ -10,87 +10,86 @@ import Key from './Key';
 import KeyFound from './KeyFound'
 
 const mapStateToProps = state => ({
-    positionPlayerOne: state.positionPlayerOne,
-    positionPlayerTwo: state.positionPlayerTwo,
-    positionKey: state.positionKey,
-    isKeyFound: state.isKeyFound,
-    grid: state.grid
+  positionPlayerOne: state.positionPlayerOne,
+  positionPlayerTwo: state.positionPlayerTwo,
+  positionKey: state.positionKey,
+  isKeyFound: state.isKeyFound,
+  grid: state.grid
 })
 
-    const Maze = ({ positionPlayerOne, positionPlayerTwo, positionKey, isKeyFound, grid, dispatch }) => {
+const Maze = ({ positionPlayerOne, positionPlayerTwo, positionKey, isKeyFound, grid, dispatch }) => {
 
-        useEffect(()=>{
-            window.addEventListener('keydown', handleKeyDown)
-            return ()=>{window.removeEventListener('keydown', handleKeyDown)}
-         },[])
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => { window.removeEventListener('keydown', handleKeyDown) }
+  }, [])
 
-        const handleKeyDown = (event) => {
-          event.preventDefault()
-            switch (event.keyCode) {
-              //Up Move
-              case 90 : 
-                dispatch({ type: 'MOVE_PLAYER_ONE_UP' });
-                break;
-              case 38 : 
-                dispatch({ type: 'MOVE_PLAYER_TWO_UP' });
-                break;
-              //Down Move  
-              case 83 :
-                  dispatch({ type: 'MOVE_PLAYER_ONE_DOWN' });
-                break;
-              case 40 :
-                  dispatch({ type: 'MOVE_PLAYER_TWO_DOWN' });
-                break;
-              //Left Move  
-              case 81 :
-                  dispatch({ type: 'MOVE_PLAYER_ONE_LEFT' });
-                break;
-              case 37 :
-                  dispatch({ type: 'MOVE_PLAYER_TWO_LEFT' });
-                break;  
-              //Right Move  
-              case 68 :
-                  dispatch({ type: 'MOVE_PLAYER_ONE_RIGHT' });
-                break;
-              case 39 :
-                  dispatch({ type: 'MOVE_PLAYER_TWO_RIGHT' });
-                break;
-            default : 
-              console.log("default");
-            }
-          }
+  const handleKeyDown = (event) => {
+    event.preventDefault()
+    switch (event.keyCode) {
+      //Up Move
+      case 90:
+        dispatch({ type: 'MOVE_PLAYER_ONE_UP' });
+        break;
+      case 38:
+        dispatch({ type: 'MOVE_PLAYER_TWO_UP' });
+        break;
+      //Down Move  
+      case 83:
+        dispatch({ type: 'MOVE_PLAYER_ONE_DOWN' });
+        break;
+      case 40:
+        dispatch({ type: 'MOVE_PLAYER_TWO_DOWN' });
+        break;
+      //Left Move  
+      case 81:
+        dispatch({ type: 'MOVE_PLAYER_ONE_LEFT' });
+        break;
+      case 37:
+        dispatch({ type: 'MOVE_PLAYER_TWO_LEFT' });
+        break;
+      //Right Move  
+      case 68:
+        dispatch({ type: 'MOVE_PLAYER_ONE_RIGHT' });
+        break;
+      case 39:
+        dispatch({ type: 'MOVE_PLAYER_TWO_RIGHT' });
+        break;
+      default:
+        console.log("default");
+    }
+  }
 
-    return (
-        <div>
-          <header className="header">THE MAZE RUNNERS</header>
-          <object type="audio/mpeg" width="0" height="0" data="fichier.mp3">
-<param name="filename" value="fichier.mp3" />
-<param name="autostart" value="true" />
-<param name="loop" value="false" />
-</object>
-            <div className="row">
-                {grid.map(cell => (
-                    <div className="case" key={cell.id} style={{
-                        borderColor: '#aa3311',
-                        borderLeftColor: cell.left !== 0 && "grey",
-                        borderTopColor: cell.top !== 0 && "grey",
-                        borderBottomColor: cell.down !== 0 && "grey",
-                        borderRightColor: cell.right !== 0 && "grey",
-                        borderLeftWidth: cell.left !== 0 ? '3px' : '3px',
-                        borderTopWidth: cell.top !== 0 ? '5px' : '5px',
-                        borderBottomWidth: cell.down !== 0 ? '5px' : '5px',
-                        borderRightWidth: cell.right !== 0 ? '3px' : '3px',
+  return (
+    <div>
+      <header className="header">THE MAZE RUNNERS</header>
+      <object type="audio/mpeg" width="0" height="0" data="fichier.mp3">
+        <param name="filename" value="fichier.mp3" />
+        <param name="autostart" value="true" />
+        <param name="loop" value="false" />
+      </object>
+      <div className="row">
+        {grid.map(cell => (
+          <div className="case" key={cell.id} style={{
+            borderColor: '#aa3311',
+            borderLeftColor: cell.left !== 0 && "grey",
+            borderTopColor: cell.top !== 0 && "grey",
+            borderBottomColor: cell.down !== 0 && "grey",
+            borderRightColor: cell.right !== 0 && "grey",
+            borderLeftWidth: cell.left !== 0 ? '3px' : '3px',
+            borderTopWidth: cell.top !== 0 ? '5px' : '5px',
+            borderBottomWidth: cell.down !== 0 ? '5px' : '5px',
+            borderRightWidth: cell.right !== 0 ? '3px' : '3px',
 
-                    }}>
-                        {cell.id === positionPlayerOne && <PlayerOne />}
-                        {cell.id === positionPlayerTwo && <PlayerTwo />}
-                        {(cell.id === positionKey && !isKeyFound) && <Key />}
-                    </div>))}                                       
-            </div>
-            {positionPlayerOne === positionKey && dispatch({type:'GET_KEY'}) && <KeyFound />}
-            {positionPlayerOne === "out" && <GameOver />}
-        </div>
-    )
+          }}>
+            {cell.id === positionPlayerOne && <PlayerOne />}
+            {cell.id === positionPlayerTwo && <PlayerTwo />}
+            {(cell.id === positionKey && !isKeyFound) && <Key />}
+          </div>))}
+      </div>
+      {positionPlayerOne === "out" && <GameOver />}
+    </div>
+  )
 }
 
 export default connect(mapStateToProps)(Maze);
