558fbe229e6edd6a1ead3ef599009dca49bd2b3b
Matthieu <45939940+mtthncls@users.noreply.github.com>
2019-07-12 14:51:41 +0200

    Update KeyFound.js

--- src/Components/KeyFound.js
+++ src/Components/KeyFound.js
@@ -4,7 +4,7 @@ import './WelcomePage.css';
 const KeyFound = () => {
     return (
             <div>
-                <p className="keyfound"> You found the key ! The Door is opened ! RUNAWAY Dam Foo !</p>
+                <p className="keyfound"> You found the key ! The Door is opened ! RUNAWAY You Fool !</p>
             </div>
     )
 }
