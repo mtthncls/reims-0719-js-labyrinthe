ce3f98a2957c27474e7a5261e8ebc519fc889f6a
Jeremy Figueiredo <figueiredojeremy@gmail.com>
2019-07-12 14:25:56 +0200

    ok

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -6,7 +6,7 @@ const WelcomePage = ({ dispatch }) => {
     return (
         <main className="welcome">
             <header className="header">THE MAZE RUNNERS</header>
-           <div className='logo'><img src='https://media0.giphy.com/media/WwfP2C3z1PO9PUzVfk/giphy.gif?cid=3640f6095bc0aa5c7a76355a4195ae6c' alt='logo' /> </div>
+           <div className='logo'><img className="homePic" src='https://media0.giphy.com/media/WwfP2C3z1PO9PUzVfk/giphy.gif?cid=3640f6095bc0aa5c7a76355a4195ae6c' alt='logo' /> </div>
 
             <div className='pave'>
                 <p>WELCOME, PLAYERS !</p>
@@ -15,6 +15,8 @@ const WelcomePage = ({ dispatch }) => {
                 <div className='rules'><p>PLAYER ONE HAVE TO GRAB THE KEY TO OPEN THE DOOR AND ESCAPE THE MAZE.</p>
                     <p>PLAYER TWO, 2 SECONDS LATER, MUST TRY TO CATCH UP PLAYER ONE BEFORE HE ESCAPES
                           .</p></div>
+                <img src="https://vignette.wikia.nocookie.net/diepio/images/9/93/WASD.png/revision/latest?cb=20181206065147" alt="keyboard" height="180vh" width="260vh"/>
+                <p>Player One <span>-</span> Player Two</p>  
             </div>
             <button className='enter'   onClick={() => dispatch({ type: 'DISPLAY_MAZE' })}>ENTER THE MAZE</button>
         </main>
